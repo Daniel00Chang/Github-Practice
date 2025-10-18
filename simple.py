5beb79108b5d0650264e2fae48f2996ec974514f
Daniel00Chang <changdaniel04@gmail.com>
2025-10-17 20:12:17 -0700

    simple add 2 numbers program

---
+++ simple.py
@@ -0,0 +1,12 @@
+
+def run_program():
+    print("simple program to add 2 numbers")
+    num1 = float(input("enter first number: "))
+    num2 = float(input("enter second number: "))
+
+    print("the sum is:", num1 + num2)
+
+
+
+if __name__ == "__main__":
+    run_program()
